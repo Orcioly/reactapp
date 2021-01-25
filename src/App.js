bd163e658b0db2ca0befd2f91697bb1b9cf000d9
Orcioly Andrade Alves <orcioly@techmicro.com.br>
2021-01-25 20:32:21 -0300

    Segundo commit do projeto para teste

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import { useEffect } from "react";
 import List from "./components/List";
 
 function App() {
-  useEffect(() => {
-    async function setUserRepositories() {
-      await localStorage.setItem("user-repositories", JSON.stringify({}));
-    }
-
-    setUserRepositories();
-  });
-
   return (
     <div>
       <h1>Hello React App World</h1>
     </div>
   );
 }
+
+// Teste no Git em tempo real
 
 export default App;
