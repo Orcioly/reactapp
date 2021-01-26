afbed0938f265b7981aba1e1ddc0a66cb3c5ac86
Orcioly Andrade Alves <orcioly@techmicro.com.br>
2021-01-26 13:20:24 -0300

    Testes com LocalStorage e sessionStorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useEffect, useState } from "react";
-import List from "./components/List";
-import api from "./components/services/api";
+import { useEffect, useState } from 'react';
+import List from './components/List';
+import api from './components/services/api';
 
 function App() {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const localStorageUserData = sessionStorage.getItem(
+      '@reactapp:GITHUB_USER_DATA',
+    );
+
+    setUserData(JSON.parse(localStorageUserData) || {});
+  });
 
   async function getUserGithubData() {
     const { data } = await api.get(username);
 
+    localStorage.setItem('@reactapp:GITHUB_USER_DATA', JSON.stringify(data));
     setUserData(data);
   }
 
