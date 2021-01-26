73351d459afe13c1a753675f52f7ac3c3506a736
Orcioly Andrade Alves <orcioly@techmicro.com.br>
2021-01-25 23:41:47 -0300

    Foi acrescentado testes da api do GitHub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import List from "./components/List";
+import api from "./components/services/api";
 
 function App() {
+  const [username, setUsername] = useState("Orcioly");
+  const [userData, setUserData] = useState({});
+
+  async function getUserGithubData() {
+    const { data } = await api.get(username);
+
+    setUserData(data);
+  }
+
   return (
     <div>
-      <h1>Hello React App World</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <button onClick={getUserGithubData}>Pesquisar usuário</button>
+      <div>{userData.name}</div>
+      <div>{userData.company}</div>
     </div>
   );
 }
